6fa3c683a92096b36e786b04abc0d9259e8f2b86
theLegasea <148804473+theLegasea@users.noreply.github.com>
2025-02-06 16:08:04 -0500

    refac: removed redundant use of inventory to products

--- Assignment1/Controllers/ProductsController.cs
+++ Assignment1/Controllers/ProductsController.cs
@@ -1,11 +1,56 @@
+using Assignment1.Data;
+using Assignment1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Assignment1.Controllers;
-
+/*
+ *  This is the employee inventory controller
+ *  It is responsible for non-client facing actions
+ */
 public class ProductsController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    public ProductsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
+    {
+        //
+        var inventory = _context.Products.ToList();
+        return View(inventory);
+    }
+    
+    [HttpGet]
+    public IActionResult Create()
     {
         return View();
     }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(product);
+    }
+    
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var product = _context.Products.Find(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
 }
