b3617b019ab6014dd87fdeddb53e9ed612daa462
Shaffaq Hai <hai.shaffaq@gmail.com>
2025-02-05 22:04:51 -0500

    added migrtions

--- Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebbbf82ea09f66ac4590be258c53f87a4cecdc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae3ab86e7806d775098ae38313cbcb165fde410")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
