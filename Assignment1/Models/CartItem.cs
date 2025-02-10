337f9eaeb84fc67d6dd28a5abc594d07bd2c5a67
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-10 11:30:16 -0500

    feat: new db design with cart TODO: migrate

---
+++ Assignment1/Models/CartItem.cs
@@ -0,0 +1,21 @@
+namespace Assignment1.Models;
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+
+    public int CartId { get; set; }
+
+    public int CartItemQuantity { get; set; }
+
+    public decimal CartItemTotalPrice { get; set; }
+
+    // foreign key properties
+
+    public int ProductId { get; set; }
+
+    // navigation properties
+
+    public Cart Cart { get; set; }
+    public Product Product { get; set; }
+}
--- Assignment1/Models/Category.cs
+++ Assignment1/Models/Category.cs
@@ -2,13 +2,11 @@ namespace Assignment1.Models;
 
 public class Category
 {
-    public Category(int categoryId, string categoryName)
-    {
-        CategoryId = categoryId;
-        CategoryName = categoryName;
-    }
-    public Category() { }
-
     public int CategoryId { get; set; }
+
     public string CategoryName { get; set; }
+
+    // navigation properties
+
+    public ICollection<Product> Products { get; set; } = new List<Product>();
 }
