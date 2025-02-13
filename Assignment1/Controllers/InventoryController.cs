c3e34574099b69cffc215b5a6939f65ed20e893f
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-13 14:33:50 -0500

    feat: new add to cart mechanism

--- Assignment1/Controllers/InventoryController.cs
+++ Assignment1/Controllers/InventoryController.cs
@@ -17,81 +17,46 @@ public class InventoryController : Controller
         _context = context;
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
-        var clientInventory = _context.Products.Include(p => p.Category).ToList();
-        // ViewBag.Cart = GetCart();
-        return View(clientInventory);
+        var products = _context.Products.Include(p => p.Category).ToList();
+        return View(products);
     }
 
-    // public IActionResult AddToCart(int id)
-    // {
-    //     var product = _context.Products.Find(id);
-    //     if (product == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //     /*
-    //      * check for active cart
-    //      * add the product to the list
-    //      * save the list back to the session
-    //      */
-    //     List<Product> cart = GetCart();
-    //     cart.Add(product);
-    //     StashCart(cart);
-    //     return RedirectToAction("Index");
-    // }
-
-    // public IActionResult RemoveFromCart(int id)
-    // {
-    //     var product = _context.Products.Find(id);
-    //     if (product == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //     List<Product> cart = GetCart();
-    //     var toRemove = cart.Find(p => p.ProductId == id);
-    //     if (toRemove != null)
-    //     {
-    //         cart.Remove(toRemove);
-    //         StashCart(cart);
-    //     }
-    //     // gross ajax stuff to stop the refresh problem with offcanvas
-    //     if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-    //     {
-    //         return Json(new { success = true });
-    //     }
-    //     return RedirectToAction("Index");
-    // }
-
-    [HttpGet]
-    public IActionResult UpdateCartQuantity(int productId, int updateDirection)
+    [HttpPost]
+    public IActionResult AddToCart(int id, int quantity)
     {
-        var product = _context.Products.Find(productId);
+        var product = _context.Products.Find(id);
         if (product == null)
         {
-            return NotFound();
+            return Content("Product not found.");
         }
 
         var cartJson = HttpContext.Session.GetString("Cart") ?? "[]";
         var cart = JsonSerializer.Deserialize<List<OrderItem>>(cartJson) ?? new List<OrderItem>();
-
-        if (cart.All(oi => oi.ProductId != productId))
+        if (cart.All(oi => oi.ProductId != product.ProductId))
         {
             cart.Add(new OrderItem {
-                ProductId = productId,
-                Quantity = 0
+                ProductId = product.ProductId,
+                Quantity = quantity
             });
         }
 
-        var cartItem = cart.Find(oi => oi.ProductId == productId);
-        cartItem.Quantity += updateDirection;
+        var cartItem = cart.Find(oi => oi.ProductId == product.ProductId);
+        cartItem.Quantity += quantity;
 
-        if (cartItem.Quantity  < 0) cartItem.Quantity = 0;
-        else if (cartItem.Quantity > product.ProductStock) cartItem.Quantity = product.ProductStock;
-
-        StashCart(cart);
-        return Content($"{cartItem.Quantity}");
+        if (cartItem.Quantity > product.ProductStock)
+        {
+            cartItem.Quantity = product.ProductStock;
+            StashCart(cart);
+            return Content($"You can't have more than {product.ProductStock} of {product.ProductName} in your cart.");
+        }
+        else
+        {
+            StashCart(cart);
+            return Content($"Successfully added {quantity} of {product.ProductName} to cart.");
+        }
     }
 
     private void StashCart(List<OrderItem> cart)
@@ -99,19 +64,4 @@ public class InventoryController : Controller
         string cartJson = JsonSerializer.Serialize(cart);
         HttpContext.Session.SetString("Cart", cartJson);
     }
-
-    // CART SESSION COOKING
-    // private List<Product> GetCart()
-    // {
-    //     string cartJson = HttpContext.Session.GetString("Cart"); // why is the syntax for this so verbose lmao
-    //     // make empty list if !cart
-    //     if(string.IsNullOrEmpty(cartJson))
-    //     {
-    //         return new List<Product>();
-    //     }
-    //     else
-    //     {
-    //         return JsonSerializer.Deserialize<List<Product>>(cartJson); // I just threw up in my mouth
-    //     }
-    // }
 }
