dcc8b714e876ed8bc03badb08241a50bdd990b66
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-14 10:29:21 -0500

    fix: add-to-cart msg is now 'successfully added [quantity]' not [total]

--- Assignment1/Controllers/ClientController.cs
+++ Assignment1/Controllers/ClientController.cs
@@ -81,7 +81,7 @@ public class ClientController : Controller
         return new JsonResult(new
         {
             success = true,
-            message = $"Successfully added {cartItem.Quantity} of {product.ProductName} to cart."
+            message = $"Successfully added {quantity} of {product.ProductName} to cart."
         });
     }
 
