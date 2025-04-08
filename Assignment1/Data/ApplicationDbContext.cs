42dafaf7683dfdf5b72e6afea05b92f641070466
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-04-08 14:43:12 -0400

    updated table names, password reset function works

--- Assignment1/Data/ApplicationDbContext.cs
+++ Assignment1/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Assignment1.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace Assignment1.Data;
@@ -11,6 +12,44 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Category> Categories{ get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Must ensure Identity's configuration are applied first
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("Identity");
     
+         
+        modelBuilder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable("User");
+        });
+        modelBuilder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable("Role");
+        });
+        modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        modelBuilder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        modelBuilder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
+        modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        modelBuilder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("UserRoleClaims");
+        });
+
+    }
+
     /*public DbSet<User> Users { get; set; }  UNDER NEW MANAGEMENT*/
 }
