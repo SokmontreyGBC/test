7103463328f92ec14e49764084daa400606dbdeb
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-09 13:30:30 -0400

    feat: move SMTP to brevo

--- Assignment1/Services/EmailSender.cs
+++ Assignment1/Services/EmailSender.cs
@@ -1,38 +1,42 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using SendGrid.Helpers.Mail;
 using SendGrid;
 namespace Assignment1.Services;
 
-public class EmailSender: IEmailSender
+public class EmailSender(IConfiguration configuration): IEmailSender
 {
-    private readonly string _sendGridApiKey;
-
-    public EmailSender(IConfiguration configuration)
-    {
-        _sendGridApiKey = configuration["SendGrid:ApiKey"] ?? throw  new ArgumentNullException("SendGrid Key is missing");
-    }
-
     public async Task SendEmailAsync(string email, string subject, string message)
     {
         try
         {
-            var client = new SendGridClient(_sendGridApiKey);
-            var from = new EmailAddress("hai.shaffaq@gmail.com","PM Tool Default Sender");
-            var to = new EmailAddress(email);
-            var msg =  MailHelper.CreateSingleEmail(from, to, subject, "Welcome to PM Tool INC", message);
-            
-            var response = await client.SendEmailAsync(msg);
-            if (!response.IsSuccessStatusCode)
+            var from = new MailboxAddress("Brother's Oats Team", "sythatsokmontrey@gmail.com");
+            var to = new MailboxAddress("", email);
+
+            var msg = new MimeMessage();
+            msg.From.Add(from);
+            msg.To.Add(to);
+            msg.Subject = subject;
+
+            msg.Body = new BodyBuilder
             {
-                var errorMessage = await response.Body.ReadAsStringAsync();
-                Console.WriteLine($"An error occured while sending an email: {errorMessage} ");
-            }
+                TextBody = "Brother's Oats Team",
+                HtmlBody = message
+            }.ToMessageBody();
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp-relay.brevo.com", 587, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(configuration["Brevo:Account"], configuration["Brevo:MasterKey"]);
+            await client.SendAsync(msg);
+            await client.DisconnectAsync(true);
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
-            Console.WriteLine($"An error occured while sending an email: {ex.Message}");
+            Console.WriteLine($"An error occurred while sending email: {ex.Message}");
             throw;
         }
+
     }
-    
 }
