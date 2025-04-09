25c120aff54edf9350e0192ffd1883baefa4b3e2
M Shaffaq Hai <hai.shaffaq@gmail.com>
2025-04-09 16:34:32 -0400

    created 400/500

--- Assignment1/Controllers/HomeController.cs
+++ Assignment1/Controllers/HomeController.cs
@@ -24,7 +24,8 @@ public class HomeController : Controller
             .OrderByDescending(p => p.ProductStock)
             .Take(3)
             .ToList();
-    
+
+      
         return View(topProducts);
     }
     
