1ce5f57ed93fdd9f2b4fa02d52d5ea1ff983c398
theLegasea <148804473+theLegasea@users.noreply.github.com>
2025-04-10 14:33:55 -0400

    PHONE is still broken, updated styles

--- Assignment1/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Assignment1/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -13,6 +13,7 @@ namespace  Assignment1.Areas.Identity.Pages.Account.Manage
     /// </summary>
     public static class ManageNavPages
     {
+        public static string Phone => "Phone";
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -65,6 +66,10 @@ namespace  Assignment1.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+     
+        public static string PhoneNavClass(ViewContext viewContext) => PageNavClass(viewContext, Phone);
+
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         /// <summary>
