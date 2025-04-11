b86c5fe4b5ba244244893e42905c08f97cd6fca1
theLegasea <148804473+theLegasea@users.noreply.github.com>
2025-04-11 14:22:59 -0400

    added name change

---
+++ Assignment1/Areas/Identity/Pages/Account/Manage/ChangeName.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1.Areas.Identity.Pages.Account.Manage;
+
+public class ChangeName : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
